710f4d654aaf4c6b9252ec670bce1203c79f3e6a
TJ <bummer@bummercloud.com>
2011-08-21 02:53:47 -0700

    changing readme to markdown

--- apc-cache-purge.php
+++ apc-cache-purge.php
@@ -14,11 +14,11 @@ Author URI: http://tjstein.com
 function apc_purge() {
 	return apc_clear_cache;
 }
-// Add Clear APC menu under Tools menu
+// Add Purge APC menu under Tools menu
 add_action('admin_menu', 'php_apc_info');
         
 function php_apc_info() {
-	add_submenu_page('tools.php', 'Flush APC', 'Flush APC', 'activate_plugins', 'flush_php_apc', 'php_apc_options');
+	add_submenu_page('tools.php', 'Purge APC', 'Purge APC', 'activate_plugins', 'flush_php_apc', 'php_apc_options');
 }
         
 function php_apc_options() {
@@ -28,11 +28,11 @@ function php_apc_options() {
 		print '<p>Clearing Failed!</p>';
 	print '<pre>'; print_r(apc_cache_info()); print '</pre>';
 }
-// Add Clear APC in the favorite actions dropdown
+// Add Purge APC in the favorite actions dropdown
 add_filter('favorite_actions', 'clear_apc_link');
         
 function clear_apc_link($actions) {
-	$actions['tools.php?page=flush_php_apc'] = array('Flush APC', 'edit_posts');
+	$actions['tools.php?page=flush_php_apc'] = array('Purge APC', 'edit_posts');
 	return $actions;
 }
 ?>
